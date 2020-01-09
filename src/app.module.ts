af03152b9f20304d64cbe0b9ee7d878d3f57028f
jeyarni <jeya08sivam@gmail.com>
2020-01-09 09:40:12 +0530

    User CRUD creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ComponentsModule } from './components/components.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/config'
 
 @Module({
-  imports: [ComponentsModule,MongooseModule.forRoot('mongodb://localhost/tourapp')],
+  imports: [ComponentsModule,MongooseModule.forRoot(config.mongoURI),],
   // imports: [ComponentsModule,MongooseModule.forRoot('mongodb+srv://user1:user1@invictatour-rrvgz.gcp.mongodb.net/test?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/components/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Delete, Param, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './user.dto/user.dto';
+import { User } from './user.interface/user.interface';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService:UsersService){}
+    @Get()
+    findAll():Promise<User[]>{
+        return this.usersService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id):Promise<User>{
+        return this.usersService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() userDto:UserDto):Promise<User>{
+        return this.usersService.create(userDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id):Promise<User>{
+        return this.usersService.delete(id)
+    }
+
+    @Put(':id')
+    update(@Body() userDto:UserDto, @Param('id') id):Promise<User>{
+        return this.usersService.update(id,userDto)
+    }
+}
