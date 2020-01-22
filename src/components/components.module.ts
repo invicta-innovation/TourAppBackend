2c29a69f30ffb4ed8837d7a80454df0d35d00c5c
thuva28 <raganthiva@gmail.com>
2020-01-22 11:38:45 +0530

    video part

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { HistoryModule } from './history/audio.module';
+import { HistoryModule } from './history/history.module';
+import { VideoModule } from './video/video.module';
+
 
 @Module({
-  imports: [UsersModule,HistoryModule]
+  imports: [UsersModule,HistoryModule, VideoModule]
 })
 export class ComponentsModule {}
